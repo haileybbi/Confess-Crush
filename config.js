09458092b84eb28a2747be5a74ef9a3f860abd7e
haileybbi <121336727+haileybbi@users.noreply.github.com>
2023-05-13 02:31:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,11 @@ const CONFIG = {
     titleWeb: "ai bik j dau",
     introTitle: 'Helo Bạn Bi',
     introDesc: `Roses are red
+    
     Violets are blue
+    
     Bạn Bi k yêu tớ
+    
    Là bạn Bi ngu`,
     btnIntro: '^^HiHi^^',
     title: 'Dở hơi vkl dhs hút cần ngồi làm trò này ạ =)))',
@@ -11,10 +14,10 @@ const CONFIG = {
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Thế có yêu ko????',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    btnReply: 'trả lời nhanh lên',
+    reply: 'Yêu vkl thề đéo yêu làm chó',
+    mess: 'Tất nhiên rồi ily too',
+    messDesc: 'nhắn rủ đi chơi đi nhanh cái tay lên',
+    btnAccept: 'Okiiiii',
+    messLink: 'https://www.facebook.com/messages/t/100004057576701' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
